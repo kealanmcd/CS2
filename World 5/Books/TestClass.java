c6ca7fcc1d1be05e5a39fcd07e5f4e725adaf9f8
Kealan McDevitt <kealanmcdevitt@shopify.com>
2021-01-18 16:11:21 +0000

    Books Test class update

--- World 5/Books/TestClass.java
+++ World 5/Books/TestClass.java
@@ -1,120 +1,70 @@
 public class TestClass {
   public static void main(String[] args) {
 
-    
-    Book book1 = new Book("The Lord of the Rings: The Fellowship of the Ring", "J. R. R. Tolkien", 1001);
-    Book book2 = new Book("The Lord of the Rings: The Two Towers", "J. R. R. Tolkien", 1002);
-    Book book3 = new Book("The Lord of the Rings: The Return of the King", "J. R. R. Tolkien", 1003);
-    Book book4 = new Book("The Hobbit", "J. R. R. Tolkien", 1004);
-    Book book5 = new Book("Harry Potter: Philosopher's Stone", "J. K. Rowling", 1005);
-    Book book6 = new Book("Harry Potter: Chamber of Secrets", "J. K. Rowling", 1006);
-    Book book7 = new Book("Harry Potter: Prisoner of Azkaban", "J. K. Rowling", 1007);
-    Book book8 = new Book("Harry Potter: Goblet of Fire", "J. K. Rowling", 1008);
-    Book book9 = new Book("Harry Potter: Order of the Phoenix", "J. K. Rowling", 1009);
-    Book book10 = new Book("Harry Potter: Half-Blood Prince", "J. K. Rowling", 1010);
-    Book book11 = new Book("Harry Potter: Deathly Hallows", "J. K. Rowling", 1011);
-    Book book12 = new Book("Brave New World", "Aldous Huxley", 1012);
-    Book book13 = new Book("To Kill a Mockingbird", "Harper Lee", 1013);
-    Book book14 = new Book("The Shining", "Stephen King", 1014);
-    Book book15 = new Book("Nineteen Eighty-Four", "George Orwell", 1015);
-    Book book16 = new Book("Animal Farm", "George Orwell", 1016);
-    Book book17 = new Book("Charlie and the Chocolate Factory", "Roald Dahl", 1017);
-    Book book18 = new Book("Matilda", "Roald Dahl", 1018);
-    Book book19 = new Book("The BFG", "Roald Dahl", 1019);
-    Book book20 = new Book("The Witches", "Roald Dahl", 1020);
-    Book book21 = new Book("The Twits", "Roald Dahl", 1021);
-    Book book22 = new Book("James and the Giant Peach", "Roald Dahl", 1022);
-    Book book23 = new Book("Fantastic Mr Fox", "Roald Dahl", 1023);
-    Book book24 = new Book("Oliver Twist", "Charles Dickens", 1024);
-    Book book25 = new Book("Great Expectations", "Charles Dickens", 1025);
-    Book book26 = new Book("The Sheep-Pig", "Dick King-Smith", 1026);
-    Book book27 = new Book("The Fox Busters", "Dick King-Smith", 1027);
-    Book book28 = new Book("The Hodgeheg", "Dick King-Smith", 1028);
-    Book book29 = new Book("Martin's Mice", "Dick King-Smith", 1029);
-    Book book30 = new Book("Invisible Dog", "Dick King-Smith", 1030);
-    Book book31 = new Book("The Water Horse", "Dick King-Smith", 1031);
-    Book book32 = new Book("The Queen's Nose", "Dick King-Smith", 1032);
-    Book book33 = new Book("Animal Stories", "Dick King-Smith", 1033);
-    Book book34 = new Book("Sophie's Snail", "Dick King-Smith", 1034);
-    Book book35 = new Book("Sophie's Tom", "Dick King-Smith", 1035);
-    Book book36 = new Book("Lady Lollipop", "Dick King-Smith", 1036);
-    Book book37 = new Book("Harriet's Hare", "Dick King-Smith", 1037);
-    Book book38 = new Book("A Mouse Called Wolf", "Dick King-Smith", 1038);
-    Book book39 = new Book("George Speaks", "Dick King-Smith", 1039);
-    Book book40 = new Book("Sophie Is Seven", "Dick King-Smith", 1040);
-    Book book41 = new Book("The Cuckoo Child", "Dick King-Smith", 1041);
-    Book book42 = new Book("Ace, the Very Important Pig", "Dick King-Smith", 1042);
-    Book book43 = new Book("The Crowstarver", "Dick King-Smith", 1043);
-    Book book44 = new Book("Sophie's Lucky", "Dick King-Smith", 1044);
-    Book book45 = new Book("The School Mouse", "Dick King-Smith", 1045);
-    Book book46 = new Book("Aristotle", "Dick King-Smith", 1046);
-    Book book47 = new Book("Titus Rules!", "Dick King-Smith", 1047);
-    Book book48 = new Book("Dinosaur Trouble", "Dick King-Smith", 1048);
-    Book book49 = new Book("Sophie Hits Six", "Dick King-Smith", 1049);
-    Book book50 = new Book("Three Terrible Trins", "Dick King-Smith", 1050);
-
-    
-
     Reader Tom = new Reader("Tom");
-
     Bookcase shelf = new Bookcase(5, 10);
 
-    shelf.placeOnShelf(book1);
-    shelf.placeOnShelf(book2);
-    shelf.placeOnShelf(book3);
-    shelf.placeOnShelf(book4);
-    shelf.placeOnShelf(book5);
-    shelf.placeOnShelf(book6);
-    shelf.placeOnShelf(book7);
-    shelf.placeOnShelf(book8);
-    shelf.placeOnShelf(book9);
-    shelf.placeOnShelf(book10);
-    shelf.placeOnShelf(book11);
-    shelf.placeOnShelf(book12);
-    shelf.placeOnShelf(book13);
-    shelf.placeOnShelf(book14);
-    shelf.placeOnShelf(book15);
-    shelf.placeOnShelf(book16);
-    shelf.placeOnShelf(book17);
-    shelf.placeOnShelf(book18);
-    shelf.placeOnShelf(book19);
-    shelf.placeOnShelf(book20);
-    shelf.placeOnShelf(book21);
-    shelf.placeOnShelf(book22);
-    shelf.placeOnShelf(book23);
-    shelf.placeOnShelf(book24);
-    shelf.placeOnShelf(book25);
-    shelf.placeOnShelf(book26);
-    shelf.placeOnShelf(book27);
-    shelf.placeOnShelf(book28);
-    shelf.placeOnShelf(book29);
-    shelf.placeOnShelf(book30);
-    shelf.placeOnShelf(book31);
-    shelf.placeOnShelf(book32);
-    shelf.placeOnShelf(book33);
-    shelf.placeOnShelf(book34);
-    shelf.placeOnShelf(book35);
-    shelf.placeOnShelf(book36);
-    shelf.placeOnShelf(book37);
-    shelf.placeOnShelf(book38);
-    shelf.placeOnShelf(book39);
-    shelf.placeOnShelf(book40);
-    shelf.placeOnShelf(book41);
-    shelf.placeOnShelf(book42);
-    shelf.placeOnShelf(book43);
-    shelf.placeOnShelf(book44);
-    shelf.placeOnShelf(book45);
-    shelf.placeOnShelf(book46);
-    shelf.placeOnShelf(book47);
-    shelf.placeOnShelf(book48);
-    shelf.placeOnShelf(book49);
-    shelf.placeOnShelf(book50);
-    
+    Book booksArray[] = new Book[] {
+        new Book("The Lord of the Rings: The Fellowship of the Ring", "J. R. R. Tolkien", 1001),
+        new Book("The Lord of the Rings: The Two Towers", "J. R. R. Tolkien", 1002),
+        new Book("The Lord of the Rings: The Return of the King", "J. R. R. Tolkien", 1003),
+        new Book("The Hobbit", "J. R. R. Tolkien", 1004),
+        new Book("Harry Potter: Philosopher's Stone", "J. K. Rowling", 1005),      
+        new Book("Harry Potter: Chamber of Secrets", "J. K. Rowling", 1006),
+        new Book("Harry Potter: Prisoner of Azkaban", "J. K. Rowling", 1007),
+        new Book("Harry Potter: Goblet of Fire", "J. K. Rowling", 1008),
+        new Book("Harry Potter: Order of the Phoenix", "J. K. Rowling", 1009),
+        new Book("Harry Potter: Half-Blood Prince", "J. K. Rowling", 1010),
+        new Book("Harry Potter: Deathly Hallows", "J. K. Rowling", 1011),
+        new Book("Brave New World", "Aldous Huxley", 1012),
+        new Book("To Kill a Mockingbird", "Harper Lee", 1013),
+        new Book("The Shining", "Stephen King", 1014),
+        new Book("Nineteen Eighty-Four", "George Orwell", 1015),
+        new Book("Animal Farm", "George Orwell", 1016),
+        new Book("Charlie and the Chocolate Factory", "Roald Dahl", 1017),
+        new Book("Matilda", "Roald Dahl", 1018),
+        new Book("The BFG", "Roald Dahl", 1019),
+        new Book("The Witches", "Roald Dahl", 1020),
+        new Book("The Twits", "Roald Dahl", 1021),
+        new Book("James and the Giant Peach", "Roald Dahl", 1022),
+        new Book("Fantastic Mr Fox", "Roald Dahl", 1023),
+        new Book("Oliver Twist", "Charles Dickens", 1024),
+        new Book("Great Expectations", "Charles Dickens", 1025),
+        new Book("The Sheep-Pig", "Dick King-Smith", 1026),
+        new Book("The Fox Busters", "Dick King-Smith", 1027),
+        new Book("The Hodgeheg", "Dick King-Smith", 1028),
+        new Book("Martin's Mice", "Dick King-Smith", 1029),
+        new Book("Invisible Dog", "Dick King-Smith", 1030),
+        new Book("The Water Horse", "Dick King-Smith", 1031),
+        new Book("The Queen's Nose", "Dick King-Smith", 1032),
+        new Book("Animal Stories", "Dick King-Smith", 1033),
+        new Book("Sophie's Snail", "Dick King-Smith", 1034),
+        new Book("Sophie's Tom", "Dick King-Smith", 1035),
+        new Book("Lady Lollipop", "Dick King-Smith", 1036),
+        new Book("Harriet's Hare", "Dick King-Smith", 1037),
+        new Book("A Mouse Called Wolf", "Dick King-Smith", 1038),
+        new Book("George Speaks", "Dick King-Smith", 1039),
+        new Book("Sophie Is Seven", "Dick King-Smith", 1040),
+        new Book("The Cuckoo Child", "Dick King-Smith", 1041),
+        new Book("Ace, the Very Important Pig", "Dick King-Smith", 1042),
+        new Book("The Crowstarver", "Dick King-Smith", 1043),
+        new Book("Sophie's Lucky", "Dick King-Smith", 1044),
+        new Book("The School Mouse", "Dick King-Smith", 1045),
+        new Book("Aristotle", "Dick King-Smith", 1046),
+        new Book("Titus Rules!", "Dick King-Smith", 1047),
+        new Book("Dinosaur Trouble", "Dick King-Smith", 1048),
+        new Book("Sophie Hits Six", "Dick King-Smith", 1049),
+        new Book("Three Terrible Trins", "Dick King-Smith", 1050)
+    };
+
+    for (int i = 0; i < 50; i++) {
+        shelf.placeOnShelf(booksArray[i]);
+    }
 
     shelf.printAllBooks();
 
     System.out.println();
-    Tom.retrieveBook(shelf, book2);
+    Tom.retrieveBook(shelf, booksArray[1]);
     System.out.println(Tom.getBook());
     System.out.println();
     System.out.println();
@@ -123,9 +73,9 @@ public class TestClass {
     System.out.println();
 
     Tom.getBook();
-    Tom.retrieveBook(shelf, book2);
+    Tom.retrieveBook(shelf, booksArray[3]);
     System.out.println(Tom.getBook());
-    Tom.retrieveBook(shelf, book13);
+    Tom.retrieveBook(shelf, booksArray[7]);
 
   }
 }
